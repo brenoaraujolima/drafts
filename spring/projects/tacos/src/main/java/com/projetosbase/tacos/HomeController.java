d4d5d38be4b5261c448f81eb2b051cad5686b471
brenoaraujolima <breno.bal.araujo@gmail.com>
2023-01-28 12:41:14 -0300

    initializing spring project

---
+++ spring/projects/tacos/src/main/java/com/projetosbase/tacos/HomeController.java
@@ -0,0 +1,11 @@
+package com.projetosbase.tacos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
