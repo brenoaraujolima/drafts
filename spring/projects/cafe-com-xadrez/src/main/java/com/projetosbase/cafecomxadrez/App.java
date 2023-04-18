99aab8df6922d3c79d8ea076af951654be2c2760
breno.araujo <breno.araujo@geosales.com.br>
2023-04-18 18:46:46 -0300

    Concluindo instanciação dos beans

--- spring/projects/cafe-com-xadrez/src/main/java/com/projetosbase/cafecomxadrez/App.java
+++ spring/projects/cafe-com-xadrez/src/main/java/com/projetosbase/cafecomxadrez/App.java
@@ -3,19 +3,11 @@ package com.projetosbase.cafecomxadrez;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.projetosbase.cafecomxadrez.entities.Casa;
-import com.projetosbase.cafecomxadrez.entities.Peca;
 import com.projetosbase.cafecomxadrez.entities.Tabuleiro;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-
-        Peca tab = (Peca) context.getBean("peaoBranco1");
-        System.out.println("Alo alo");
+        Tabuleiro tab = (Tabuleiro) context.getBean("tabuleiro");
     }
 }
