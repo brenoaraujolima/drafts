944342e10f6bcc44a608ada6459b49686a1c6d77
breno.araujo <breno.araujo@geosales.com.br>
2023-12-25 16:38:44 -0300

    presenteza

---
+++ spring/projects/presenteza/src/main/java/com/presenteza/PresentezaApplication.java
@@ -0,0 +1,13 @@
+package com.presenteza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentezaApplication.class, args);
+	}
+
+}
---
+++ spring/projects/presenteza/src/test/java/com/presenteza/PresentezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presenteza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
