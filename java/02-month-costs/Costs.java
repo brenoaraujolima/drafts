f5059c066b49d9686d7aee8751f59be948eb32cb
brenoaraujolima <breno.bal.araujo@gmail.com>
2024-01-13 18:31:34 -0300

    Beggining java journey

---
+++ java/02-month-costs/Costs.java
@@ -0,0 +1,12 @@
+public class Costs {
+    public static void main(String[] args) {
+        int januaryCost = 15000;
+        int februaryCost = 23000;
+        int marchCost = 17000;
+
+        int totalCost = januaryCost + februaryCost + marchCost;
+
+        System.out.println(totalCost);
+
+    }
+}
